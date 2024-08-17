c8797a76603231e4f20a340ab8a8bd713f82e377
Faten Sultan <1202750@student.birzeit.edu>
2024-08-17 13:15:44 +0300

    first commit

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EF_task1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_task1.Context
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=ASP9_Product;Integrated Security=True;TrustServerCertificate=True ");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
